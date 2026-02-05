b1ae5fbd4ad2bb00e9b8a85e7c40e682c109baa5
hagay kohelet <hagay2506@gmail.com>
2026-02-05 17:37:55 +0200

    exam

---
+++ script.js
@@ -0,0 +1,117 @@
+const maxScore = 100
+let player1TotalScore = 0
+let player2TotalScore = 0
+let shotPlayer1 = 0
+let shotPlayer2 = 0
+const rolling = document.getElementById("roll")
+const scorePlayer1 = document.getElementById("scorePlayer1")
+const totalScorePlayer1 = document.getElementById("totalScorePlayer1")
+const scorePlayer2 = document.getElementById("scoreplayer2")
+const totalScoreplayer2 = document.getElementById("totalScoreplayer2")
+const holdButton = document.getElementById("hold")
+const players = {
+    player1: false,
+    shots1: 0,
+    player2: false,
+    shots2: 0
+}
+
+
+
+function choosePlayer() {
+    let player = Math.floor(Math.random() * 2) + 1
+    if (player === 1) {
+        players.player1 = true
+    }
+    else {
+        players.player2 = true
+    }
+
+}
+
+
+
+function turn(players) {
+    if (players.player1) {
+        return "player1"
+    }
+    return "player2"
+}
+
+function rollDice() {
+    let dice1 = Math.floor(Math.random() * 7) + 1
+    let dice2 = Math.floor(Math.random() * 7) + 1
+    if (dice1 === dice2) {
+        return 0
+    }
+    return dice1 + dice2
+}
+
+function runGame() {
+    if (players.player1) {
+        if (players.shots1 < 5) {
+            let score = rollDice()
+            if (score === 0) {
+                holdButton.classList = "allowed"
+            }
+            
+            player1TotalScore += score
+            scorePlayer1.innerHTML = score
+            players.shots1++
+        }
+        if (players.shots1 === 5) {
+            totalScorePlayer1.innerHTML = player1TotalScore
+            rolling.classList = "hide"
+            holdButton.classList = "allowed"
+            scorePlayer1.innerHTML = 0
+        }
+        if (players.shots1 === 5 && player1TotalScore >= 100) {
+            window.alert("player 1 win")
+        }
+    }
+    else {
+        if (players.shots2 < 5) {
+            let score = rollDice()
+            if(score === 0 ) {
+                holdButton.classList = "allowed"
+            }
+            player2TotalScore += score
+            scorePlayer2.innerHTML = score
+            players.shots2++
+        }
+        if (players.shots2 === 5) {
+            totalScoreplayer2.innerHTML = player2TotalScore
+            rolling.classList = "hide"
+            holdButton.classList = "allowed"
+            scorePlayer2.innerHTML = 0
+        }
+        if (players.shots2 === 5 && player2TotalScore >= 100) {
+            
+             window.alert("player 2 win")
+        }
+    }
+
+}
+
+choosePlayer()
+holdButton.classList = "hide"
+rolling.addEventListener("click", () => {
+    runGame()
+})
+
+holdButton.addEventListener("click", () => {
+    if (players.player1) {
+        players.player1 = false
+        players.shots1 = 0
+        players.player2 = true
+        players.shots2 = 0
+    }
+    else {
+        players.player1 = true
+        players.player2 = false
+        players.shots2 = 0
+        players.shots1 =0
+    }
+    holdButton.classList = "hide"
+    rolling.classList = "allowed"
+})
